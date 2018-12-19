44601fce3f0a0afe9abe5cf5a6b0d665bf70685a
mohamadkhalil88 <mohamad.khalil88@hotmail.com>
2018-12-19 14:36:01 +0200

    Example 1 - refactoring files

---
+++ src/app/index.ts
@@ -0,0 +1,20 @@
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { fakeBackendProvider } from './_helpers';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { ExamplesComponent } from './examples';
+
+export {
+  AppComponent ,
+  AlertComponent ,
+  JwtInterceptor ,
+  ErrorInterceptor ,
+  fakeBackendProvider ,
+  HomeComponent ,
+  LoginComponent ,
+  RegisterComponent ,
+  ExamplesComponent
+};
