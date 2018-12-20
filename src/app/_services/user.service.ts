c41ed36a60a75e76b360c64fd18755c922910514
mohamadkhalil88 <mohamad.khalil88@hotmail.com>
2018-12-20 13:58:12 +0200

    Example 1 - fix environment.prod.ts

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class UserService {
     constructor(private http: HttpClient) { }
-  apiUrl
+
     getAll() {
         return this.http.get<User[]>(`${environment.apiUrl}/users`);
     }
